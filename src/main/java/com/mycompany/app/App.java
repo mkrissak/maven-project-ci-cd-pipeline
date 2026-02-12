ebaed4c0735e92ccbb79b2bc02a046fa182e6c5a
Marek Krissak <marek.krissak@gmail.com>
2026-02-12 18:42:22 +0100

    added new line to output

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,5 +6,6 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Something new to check if commited changes are automatically deployed");
     }
 }
