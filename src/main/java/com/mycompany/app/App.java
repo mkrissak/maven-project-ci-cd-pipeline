62a166bf13592111c7301c0a96bb7aae0b8f5685
Marek Krissak <marek.krissak@gmail.com>
2026-02-12 19:39:55 +0100

    - added third line to output

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Something new to check if commited changes are automatically deployed");
+        System.out.println("Third line to check if commited changes are automatically deployed");
     }
 }
